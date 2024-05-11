2865038727ccc1aa162f4ef540cc3114e09e12a1
4e4evi4ka <katazareckaa2@gmail.com>
2024-05-11 20:41:35 +0300

    JS5

--- JS5/script.js
+++ JS5/script.js
@@ -1,53 +1,55 @@
-function createNewUser(){
-    let newUser = {
-        getLogin: function() {
-            return console.log(this.Name[0].toLowerCase() + this.SName.toLowerCase());
+const createNewUser = () => {
+    const newUser = {
+        getLogin() {
+            return console.log(`${this.Name[0].toLowerCase()}${this.SName.toLowerCase()}`);
         },
-        getAge: function(){
-            let now = new Date();
-            let currentYear = now.getFullYear();
+        getAge() {
+            const now = new Date();
+            const currentYear = now.getFullYear();
 
-            let inputDate = +this.birthday.substring(0,2);
-            let inputMonth = +this.birthday.substring(3,5);
-            let inputYear = +this.birthday.substring(6,10);
+            const inputDate = +this.birthday.substring(0, 2);
+            const inputMonth = +this.birthday.substring(3, 5);
+            const inputYear = +this.birthday.substring(6, 10);
 
-            let birthDate = new Date(inputYear, inputMonth-1, inputDate);
-            let birthYear = birthDate.getFullYear();
-            let age = currentYear - birthYear;
-            if(now < new Date(birthDate.setFullYear(currentYear))){
-                age = age-1;
+            const birthDate = new Date(inputYear, inputMonth - 1, inputDate);
+            let age = currentYear - birthDate.getFullYear();
+            if (now < new Date(birthDate.setFullYear(currentYear))) {
+                age = age - 1;
             }
             return console.log(age);
         },
-        getPassword: function(){
-            return console.log(this.Name[0].toLowerCase() + this.SName.toLowerCase() + this.birthday.substring(6,10));
+        getPassword() {
+            return console.log(`${this.Name[0].toLowerCase()}${this.SName.toLowerCase()}${this.birthday.substring(6, 10)}`);
         }
     };
-    newUser.prototype = {                           
-        get username() {                            
-                return this.Name;},                 
 
-        set username(UserNames) {                   
-            this.Name = Name;},                     
-
-        get usersecondname() {                      
-                return this.SName;},                
-        
-        set usersecondname(UserSecondNames) {       
-            this.SName = SName;}                    
-        
+    newUser.prototype = {
+        get username() {
+            return this.Name;
+        },
+        set username(UserNames) {
+            this.Name = Name;
+        },
+        get usersecondname() {
+            return this.SName;
+        },
+        set usersecondname(UserSecondNames) {
+            this.SName = SName;
+        }
     };
-    newUser.Name = prompt("Введіть ім`я", 'ім`я');
-    while (newUser.Name === ''){
-        newUser.Name = prompt('Введіть ім`я ще раз!: ', 'ім`я');
 
+    newUser.Name = prompt("Введіть ім`я", 'ім`я');
+    while (newUser.Name === '') {
+        newUser.Name = prompt('Введіть ім`я ще раз!: ', 'ім`я');
     }
+
     newUser.SName = prompt("Введіть прізвище", 'прізвище');
-    while (newUser.SName === ''){
+    while (newUser.SName === '') {
         newUser.SName = prompt('Введіть прізвище ще раз!: ', 'прізвище');
     }
+
     newUser.birthday = prompt("Напишіть вашу дату народження дд.мм.рр: ", 'дд.мм.рр');
-    
+
     console.log(`Ваше ім'я: ${newUser.Name}`);
     console.log(`Ваше прізвище: ${newUser.SName}`);
     console.log(`Ваша дата народження: ${newUser.birthday}`);
@@ -55,5 +57,5 @@ function createNewUser(){
     newUser.getAge();
     newUser.getPassword();
     return newUser;
-}       
+};
 createNewUser();
