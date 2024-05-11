70f96a224ba4f329369a5218dcc660d54f1cb552
4e4evi4ka <katazareckaa2@gmail.com>
2024-05-11 20:41:47 +0300

    JS6

---
+++ JS6/script.js
@@ -0,0 +1,11 @@
+const filterBy = (array, type) => {
+    let filtered = array.filter(item => typeof item !== type);
+    return{
+        original: array, filtered: filtered
+    };
+    
+}
+let array = ['hello', 'world', 23, '23', null];
+let result = filterBy(array, 'string');
+console.log('Данні: ', result.original);
+console.log('Результат: ', result.filtered);
